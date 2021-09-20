048feda1ee595a3621ff5018104b9d43408591f5
James Cox <james@imaj.es>
2021-09-20 12:16:54 -0500

    add config for caching

--- superset_config.py
+++ superset_config.py
@@ -29,3 +29,50 @@ ENABLE_PROXY_FIX = True
 SQLLAB_ASYNC_TIME_LIMIT_SEC = 300
 SQLLAB_TIMEOUT = 300
 SUPERSET_WEBSERVER_TIMEOUT = 300
+
+# configure caching
+CACHE_CONFIG = {
+    'CACHE_TYPE': 'redis',
+    'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 24, # 1 day default (in secs)
+    'CACHE_KEY_PREFIX': 'superset_metadata',
+    'CACHE_REDIS_URL': os.environ['REDIS_URL'],
+}
+DATA_CACHE_CONFIG = {
+    'CACHE_TYPE': 'redis',
+    'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 24, # 1 day default (in secs)
+    'CACHE_KEY_PREFIX': 'superset_results',
+    'CACHE_REDIS_URL': os.environ['REDIS_URL'],
+}
+
+# config for async work
+class CeleryConfig(object):
+    BROKER_URL = os.environ['HEROKU_REDIS_AMBER_URL']
+    CELERY_IMPORTS = (
+        'superset.sql_lab',
+        'superset.tasks',
+    )
+    CELERY_RESULT_BACKEND = os.environ['HEROKU_REDIS_AMBER_URL']
+    CELERYD_LOG_LEVEL = 'DEBUG'
+    CELERYD_PREFETCH_MULTIPLIER = 10
+    CELERY_ACKS_LATE = True
+    CELERY_ANNOTATIONS = {
+        'sql_lab.get_sql_results': {
+            'rate_limit': '100/s',
+        },
+        'email_reports.send': {
+            'rate_limit': '1/s',
+            'time_limit': 120,
+            'soft_time_limit': 150,
+            'ignore_result': True,
+        },
+    }
+    CELERYBEAT_SCHEDULE = {
+        'email_reports.schedule_hourly': {
+            'task': 'email_reports.schedule_hourly',
+            'schedule': crontab(minute=1, hour='*'),
+        },
+    }
+
+CELERY_CONFIG = CeleryConfig
+from cachelib.redis import RedisCache
+RESULTS_BACKEND = RedisCache(os.environ['REDIS_URL'])
